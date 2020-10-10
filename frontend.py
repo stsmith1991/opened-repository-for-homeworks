618dab0a902bfc00e26050815e481e94318f25ce
stsmith1991 <38431753+stsmith1991@users.noreply.github.com>
2020-10-10 17:58:23 +0500

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,5 @@
+from homework4.py import imported()
+from sys import argv
+
+print(f"Введено значение отрабоанных часов {profit}\n Введена оплата одного часа:{hour_price}\n Введена премия {adv}\n "
+      f"Итоговая зарплата составляет: {result})
