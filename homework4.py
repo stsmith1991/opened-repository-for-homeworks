e9e27a33c222835ee04101df9765b607b05514f4
stsmith1991 <38431753+stsmith1991@users.noreply.github.com>
2020-10-13 23:43:20 +0500

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,15 +1,30 @@
 from functools import reduce
 from sys import argv
+from random import randint
+from itertools import islice, cycle, count
+
+
+def randomize():
+    randlist = [randint(0, 25) for el in range(randint(5, 17))]
+    return randlist
+
+
+'''Вставляет на место размещения список случайной длинны со случайными целыми числами'''
+
 
 # Задание 1.
-def imported():
-    scr_name, profit, hour_price, adv = argv
-    result = int(profit) * int(profit) + adv
-    return result
+def wage():
+    try:
+        hours, hour_price, adv = map(float, argv[1:])
+        print(f'Зарплата составляет: {hours * hour_price + adv}')
+    except ValueError:
+        print('Введите все три числовых параметра.')
 
 
-# Задание 2. Рэндомизировать список
-original_list =  [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+wage()
+
+# Задание 2.
+original_list = randomize()
 original_list.insert(0, int(0))
 my_list = [original_list[i] for i in range(len(original_list)) if original_list[i] > original_list[i - 1]]
 print(my_list)
@@ -17,10 +32,43 @@ print(my_list)
 # Задание 3.
 print([u for u in range(20, 241) if u % 20 == 0 or u % 21 == 0])
 
-# Задиние 4. Рэндомизировать исходный, упихать в одну строку
-natural_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+# Задиние 4.
+natural_list = randomize()
 print([z for z in natural_list if natural_list.count(z) == 1])
 
 # Задание 5.
 row = [i for i in range(100, 1001) if i % 2 == 0]
 print(reduce(lambda f, s: f * s, row))
+
+
+# Задание 6.
+def clone(start, finish, loops):
+    try:
+        start, finish, loops = int(start), int(finish), int(loops)
+        initial = [el for el in islice(count(), start, finish + 1)]
+        iteration = iter(el for el in cycle(initial))
+        clone_list = [next(iteration) for _ in range(loops)]
+        print(initial)
+        return clone_list
+    except ValueError:
+        return "Введено недопустимое значение"
+    except TypeError:
+        return "У вас опечатка"
+
+
+print(clone(input('Введите число с которого начнём'), input("Введите число, которым закончим"),
+            input("повторов изначальнго цикла: ")))
+
+
+# Задание 7.
+def factorial(n):
+    first_ = 1
+    if n == 0:
+        yield f"{n} =1"
+    for i in range(1, (n + 1)):
+        first_ *= 1
+        yield f'{i}!={first_}'
+
+
+for el in factorial(int(input('X!='))):
+    print(el)
